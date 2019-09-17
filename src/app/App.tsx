a5669dcddc8f77e71ba7b808d8ff2a8db23fbb3d
Satoru Yasukawa <saccho956@yahoo.co.jp>
2019-09-17 20:14:07 +0900

    [update] App.tsxでheaderを表示するように変更

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import header from '../assets/header.jpg';
 import Sidebar from './components/Sidebar';
 import Profile from './pages/Profile';
+
+type StyleProps = {
+  headerUrl: string;
+}
 
 const App: React.FC = () => {
   return (
     <Parent>
       <Sidebar />
       <Body>
+        <HeaderImg headerUrl={header} />
         <Profile />
       </Body>
     </Parent>
@@ -19,6 +25,15 @@ const Parent = styled.div`
   min-height: 100vh;
   width: 100vw;
   display: flex;
+`
+
+const HeaderImg = styled.div`
+  height: calc(100vh - 190px);
+  background-image: url(${(props: StyleProps) => props.headerUrl});
+  background-position: top right;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: calc(100vw - 220px);
 `
 
 const Body = styled.div`
--- src/app/pages/Profile.tsx
+++ src/app/pages/Profile.tsx
@@ -7,96 +7,81 @@ import Skills from '../components/Skills';
 import Conferences from '../components/Conferences';
 import Awards from '../components/Awards';
 import itsme from '../../assets/itsme.jpg';
-import header from '../../assets/header.jpg';
 import twitter from '../../assets/icons/twitter.png';
 import github from '../../assets/icons/github.png';
 import kaggle from '../../assets/icons/kaggle.png';
 import signate from '../../assets/icons/signate.png';
 
-type StyleProps = {
-  headerUrl: string;
-}
-
 const Profile: React.FC = () => {
   return(
     <div>
-      <HeaderImg headerUrl={header} />
-        <AboutMe>
-          <MyImg src={itsme} className='itsme' alt='itsme' />
-          <Bio>
-            <Name>Satoru Yasukawa / 安川 悟</Name>
-            <Status>Student at Niigata University</Status>
-            <Links>
-              <ImageLink className='twitter' href='https://twitter.com/runn_622v' iconURL={twitter} />
-              <ImageLink className='github' href='https://github.com/saccho' iconURL={github} />
-              <ImageLink className='kaggle' href='https://www.kaggle.com/saccho' iconURL={kaggle} />
-              <ImageLink className='signate' href='https://signate.jp/users/33557/history' iconURL={signate} />
-            </Links>
-          </Bio>
-        </AboutMe>
-        <DescriptionWrapper>
-          <Description>
-            <ProfileCard title='EXPERIENCES'>
-              <Experience
-                date='Apr. 2019 - present'
-                name='IEEE Shin-etsu Section Student Branch'
-                status='Vice-Chair'
-              />
-              <Experience
-                date='Jun 2019 - present'
-                name='Fuller, Inc.'
-                status='Part-time Job (Android Engineer)'
-              />
-              <Experience
-                date='Apr. 2019 - May 2019'
-                name='Fuller, Inc.'
-                status='Internship (Android Engineer)'
-              />
-              <Experience
-                date='Sep. 2018 - Sep. 2018'
-                name='Excite Japan Co., Ltd.'
-                status='Internship (Blockchain Engineer)'
-              />
-            </ProfileCard>
-            <ProfileCard title='EDUCATION'>
-              <Experience
-                date='Apr. 2018 - present'
-                name='Graduate School of Science and Technology, Niigata University'
-                status='Master’s course student'
-              />
-              <Experience
-                date='Apr. 2014 - Mar. 2018'
-                name='Niigata University'
-                status='Bachelor of Engineering'
-              />
-            </ProfileCard>
-            <ProfileCard title='STUDY'>
-              <Conferences />
-            </ProfileCard>
-            <ProfileCard title='AWARDS'>
-              <Awards />
-            </ProfileCard>
-          </Description>
-          <Description>
-            <ProfileCard title='SKILLS'>
-              <Skills />
-            </ProfileCard>
-          </Description>
-        </DescriptionWrapper>
+      <AboutMe>
+        <MyImg src={itsme} className='itsme' alt='itsme' />
+        <Bio>
+          <Name>Satoru Yasukawa / 安川 悟</Name>
+          <Status>Student at Niigata University</Status>
+          <Links>
+            <ImageLink className='twitter' href='https://twitter.com/runn_622v' iconURL={twitter} />
+            <ImageLink className='github' href='https://github.com/saccho' iconURL={github} />
+            <ImageLink className='kaggle' href='https://www.kaggle.com/saccho' iconURL={kaggle} />
+            <ImageLink className='signate' href='https://signate.jp/users/33557/history' iconURL={signate} />
+          </Links>
+        </Bio>
+      </AboutMe>
+      <DescriptionWrapper>
+        <Description>
+          <ProfileCard title='EXPERIENCES'>
+            <Experience
+              date='Apr. 2019 - present'
+              name='IEEE Shin-etsu Section Student Branch'
+              status='Vice-Chair'
+            />
+            <Experience
+              date='Jun 2019 - present'
+              name='Fuller, Inc.'
+              status='Part-time Job (Android Engineer)'
+            />
+            <Experience
+              date='Apr. 2019 - May 2019'
+              name='Fuller, Inc.'
+              status='Internship (Android Engineer)'
+            />
+            <Experience
+              date='Sep. 2018 - Sep. 2018'
+              name='Excite Japan Co., Ltd.'
+              status='Internship (Blockchain Engineer)'
+            />
+          </ProfileCard>
+          <ProfileCard title='EDUCATION'>
+            <Experience
+              date='Apr. 2018 - present'
+              name='Graduate School of Science and Technology, Niigata University'
+              status='Master’s course student'
+            />
+            <Experience
+              date='Apr. 2014 - Mar. 2018'
+              name='Niigata University'
+              status='Bachelor of Engineering'
+            />
+          </ProfileCard>
+          <ProfileCard title='STUDY'>
+            <Conferences />
+          </ProfileCard>
+          <ProfileCard title='AWARDS'>
+            <Awards />
+          </ProfileCard>
+        </Description>
+        <Description>
+          <ProfileCard title='SKILLS'>
+            <Skills />
+          </ProfileCard>
+        </Description>
+      </DescriptionWrapper>
     </div>
   );
 }
 
 /* Styles */
-const HeaderImg = styled.div`
-  height: calc(100vh - 190px);
-  background-image: url(${(props: StyleProps) => props.headerUrl});
-  background-position: top right;
-  background-attachment: fixed;
-  background-repeat: no-repeat;
-  background-size: calc(100vw - 220px);
-`
-
 const AboutMe = styled.div`
   height: 150px;
   display: flex;
