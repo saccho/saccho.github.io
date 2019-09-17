0fe4908d5cab86797d7a7a656956516b5ff38e88
Satoru Yasukawa <saccho956@yahoo.co.jp>
2019-09-17 16:41:18 +0900

    [update] Studyの内容を修正

--- src/app/components/Conference.tsx
+++ src/app/components/Conference.tsx
@@ -18,22 +18,22 @@ const conferences = {
   ],
   domestic: [
     {
-      authors: '安川悟, 金ミンソク',
-      title: '市販の無線LANを用いた屋内測位法の検討',
-      publication: '電子情報通信学会ソサイエティ大会 B-1-126',
-      date: '2019年9月'
+      authors: 'Satoru Yasukawa, Minseok Kim',
+      title: 'Indoor Localization Method using Commercial Off-The-Shelf Wireless LANs',
+      publication: '2019 IEICE Society Conference, B-1-126',
+      date: 'Sep. 2019'
     },
     {
-      authors: '安川悟, 金ミンソク',
-      title: '無線通信路の時空間特徴量を用いた屋内測位法の検討',
-      publication: '電子情報通信学会総合大会 B-1-140',
-      date: '2019年3月'
+      authors: 'Satoru Yasukawa, Minseok Kim',
+      title: 'Indoor localization Method using Spatio-Temporal Feature of Radio Propagation Characteristics',
+      publication: '2019 IEICE General Conference, B-1-140',
+      date: 'Mar. 2019'
     },
     {
-      authors: '安川悟, 金ミンソク',
-      title: '通信路の時空間特徴量を用いたWiFi測位システムの検討',
-      publication: '電子情報通信学会信越支部大会 4B-3',
-      date: '2018年9月'
+      authors: 'Satoru Yasukawa, Minseok Kim',
+      title: 'A Study on Positioning System with WiFi using Spetiotemporal Feature of Communication Path',
+      publication: '2018 IEICE Shinetsu Branch Conference, 4B-3',
+      date: 'Sep. 2018'
     },
     {
       authors: '安川悟, 金ミンソク',
@@ -41,12 +41,21 @@ const conferences = {
       publication: '電子情報通信学会 新潟大学 学生ブランチ',
       date: '2017年12月'
     },
+  ],
+  tech: [
+    {
+      authors: 'Hideaki Momose, Satoru Yasukawa, Minseok Kim',
+      title: 'Developement of MIMO Channel Sounder using Commercial Off-The-Shelf Wireless LANs',
+      publication: 'IEICE Technical Report, AP2019-25',
+      date: 'Jul. 2019'
+    }
   ]
 }
 
 const Conference: React.FC = () => {
   const iConfList = []
   const dConfList = []
+  const dTechList = []
 
   for(const confIdx in conferences.international){
     const iConf = conferences.international[confIdx]
@@ -58,12 +67,22 @@ const Conference: React.FC = () => {
       </ConfUl>
     )
   }
-  for(const i in conferences.domestic){
-    const dConf = conferences.domestic[i]
+  for(const confIdx in conferences.domestic){
+    const dConf = conferences.domestic[confIdx]
     dConfList.push(
       <ConfUl>
         <ConfLi>
           {dConf.authors}, "{dConf.title}," {dConf.publication}, {dConf.date}.
+        </ConfLi>
+      </ConfUl>
+    )
+  }
+  for(const techIdx in conferences.tech){
+    const tech = conferences.tech[techIdx]
+    dTechList.push(
+      <ConfUl>
+        <ConfLi>
+          {tech.authors}, "{tech.title}," {tech.publication}, {tech.date}.
         </ConfLi>
       </ConfUl>
     )
@@ -76,8 +95,12 @@ const Conference: React.FC = () => {
         {iConfList}
       </Conf>
       <Conf>
-        Domestic conference
+        Domestic conference (Japanese)
         {dConfList}
+      </Conf>
+      <Conf>
+        IEICE Technical Reports (Japanese)
+        {dTechList}
       </Conf>
     </Wrapper>
   );
