1f35d7455bce5e47eacc3e7c945430c331d7565d
saccho <saccho956@yahoo.co.jp>
2019-11-26 16:32:38 +0900

    [clean] format code

--- src/app/components/WithTracker.tsx
+++ src/app/components/WithTracker.tsx
@@ -4,35 +4,35 @@ import * as GoogleAnalytics from "react-ga";
 GoogleAnalytics.initialize(process.env.REACT_APP_GA_TRACK_ID);
 
 const WithTracker = (WrappedComponent: React.FC, options = {}) => {
-    const trackPage = (page: string) => {
-        GoogleAnalytics.set({
-            page,
-            ...options
-        });
-        GoogleAnalytics.pageview(page);
-    };
+  const trackPage = (page: string) => {
+    GoogleAnalytics.set({
+      page,
+      ...options
+    });
+    GoogleAnalytics.pageview(page);
+  };
 
-    const HOC = class extends React.Component<any, any> {
-        componentDidMount() {
-            const page = this.props.location.pathname;
-            trackPage(page);
-        }
+  const HOC = class extends React.Component<any, any> {
+    componentDidMount() {
+      const page = this.props.location.pathname;
+      trackPage(page);
+    }
 
-        componentWillReceiveProps(nextProps: any) {
-            const currentPage = this.props.location.pathname;
-            const nextPage = nextProps.location.pathname;
+    componentWillReceiveProps(nextProps: any) {
+      const currentPage = this.props.location.pathname;
+      const nextPage = nextProps.location.pathname;
 
-            if (currentPage !== nextPage) {
-                trackPage(nextPage);
-            }
-        }
+      if (currentPage !== nextPage) {
+        trackPage(nextPage);
+      }
+    }
 
-        render() {
-            return <WrappedComponent {...this.props} />;
-        }
-    };
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
 
-    return HOC;
+  return HOC;
 };
 
 export default WithTracker;
