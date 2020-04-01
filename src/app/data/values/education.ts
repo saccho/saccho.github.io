3aea12e8085259b22ed72df5f9958971b405ceb8
saccho <saccho956@yahoo.co.jp>
2020-04-02 01:12:24 +0900

    update education

--- src/app/data/values/education.ts
+++ src/app/data/values/education.ts
@@ -1,8 +1,8 @@
 const education = [
   {
-    date: "Apr. 2018 - present",
+    date: "Apr. 2018 - Mar. 2020",
     name: "Graduate School of Science and Technology, Niigata University",
-    status: "Master’s course student"
+    status: "Master of Engineering"
   },
   {
     date: "Apr. 2014 - Mar. 2018",
